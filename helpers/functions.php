a04759747e4b7dbc826c1b80e4da8941a1dfd6e8
giuseppesciacca <sciaccagi@gmail.com>
2023-05-05 17:53:16 +0200

    [fix] case if password_length = null

--- helpers/functions.php
+++ helpers/functions.php
@@ -9,10 +9,13 @@ $charachter = array_merge($alphabet_up, $alphabet_lw, $numbers);
 shuffle($charachter); //mischio l'array
 
 //prendo casualmente degli indici dall'array
-$random_index_charachter = array_rand($alphabet_up, $password_length);
 
 $password_for_user = '';
-foreach ($random_index_charachter as $key => $value) {
-    $password_for_user .= $charachter[$value];
+if ($password_length != null) {
+    $random_index_charachter = array_rand($charachter, $password_length);
+
+    foreach ($random_index_charachter as $key => $value) {
+        $password_for_user .= $charachter[$value];
+    }
 }
 //var_dump($password_for_user);
